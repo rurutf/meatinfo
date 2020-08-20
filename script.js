c7f9e87a6e222e9326658a093990352470e15458
Inna Pavlyutenkova <61685066+rurutf@users.noreply.github.com>
2020-08-20 12:44:53 +0300

    Add files via upload
    "Добавлена адаптивная верстка"

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//let question = document.getElementById('question-link');
+//Кнопка "Работаете в этой компании?"
 function ChangeText (btn) {
     //document.getElementById("question-link").innerHTML="Отменить запрос в сотрудники";
     if (btn.innerHTML == 'Работаете в этой компании?') {
@@ -20,3 +20,45 @@ function ChangeText (btn) {
         btn.className = "question-link";
     }
 }
+
+//Всплывающая подсказка
+let tooltipElem;
+
+    document.onmouseover = function(event) {
+    let target = event.target;
+
+      // если у нас есть подсказка...
+    let tooltipHtml = target.dataset.tooltip;
+    if (!tooltipHtml) return;
+
+      // ...создадим элемент для подсказки
+
+    tooltipElem = document.createElement('div');
+    tooltipElem.className = 'tooltip';
+    tooltipElem.innerHTML = tooltipHtml;
+    document.body.append(tooltipElem);
+
+      // спозиционируем его сверху от аннотируемого элемента (top-center)
+    let coords = target.getBoundingClientRect();
+
+    let left = coords.left + (target.offsetWidth - tooltipElem.offsetWidth) / 2;
+      if (left < 0) left = 0; // не заезжать за левый край окна
+
+    let top = coords.top - tooltipElem.offsetHeight - 5;
+      if (top < 0) { // если подсказка не помещается сверху, то отображать её снизу
+        top = coords.top + target.offsetHeight + 5;
+    }
+
+    tooltipElem.style.left = left + 'px';
+    tooltipElem.style.top = top + 'px';
+    };
+
+    document.onmouseout = function(e) {
+
+    if (tooltipElem) {
+        tooltipElem.remove();
+        tooltipElem = null;
+    }
+
+    };
+
